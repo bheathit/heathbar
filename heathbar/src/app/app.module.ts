2ee3c42f1df65e415fc6a5841862ebcab0cabd72
Benjamin Heath <bheathit@gmail.com>
2020-03-12 20:12:50 -0400

    Initial setup part two

---
+++ heathbar/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule, routes } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutPageComponent,
+    HomePageComponent,
+    ContactPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
